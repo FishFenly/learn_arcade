6640dabe67e2bb860e182e24f554cf7f0f2472c1
Joseph Fenly <joseph.fenly@northumbria.ac.uk>
2019-02-08 20:27:59 +0000

    complete

--- learn_arcade/draw_sample_lab.py
+++ learn_arcade/draw_sample_lab.py
@@ -17,15 +17,43 @@ def app():
         0,
         (59,192,52)
     )
+    # draw the sun
+    arcade.draw_circle_filled(
+        c.SCREEN_WIDTH / 1.3,
+        c.SCREEN_HEIGHT / 1.1,
+        50,
+        (231,229,88)
+    )
 
-    # draw the first tree
+    # draw the first tree trunk
     arcade.draw_rectangle_filled(
-        c.SCREEN_WIDTH - 100,
-        c.SCREEN_HEIGHT - 100,
-        10,
-        10,
-        (),
-        90
+        c.SCREEN_WIDTH / 6,
+        c.SCREEN_HEIGHT / 2,
+        30,
+        300,
+        (92,76,62)
+    )
+    # first tree leaves
+    arcade.draw_circle_filled(
+        c.SCREEN_WIDTH / 6,
+        c.SCREEN_HEIGHT / 2 + 150,
+        100,
+        (51,100,54)
+    )
+    # second tree trunk
+    arcade.draw_rectangle_filled(
+        c.SCREEN_WIDTH / 1.2,
+        c.SCREEN_HEIGHT / 2,
+        30,
+        300,
+        (92,76,62)
+    )
+    # second tree leaves
+    arcade.draw_circle_filled(
+        c.SCREEN_WIDTH / 1.2,
+        c.SCREEN_HEIGHT / 2 + 150,
+        100,
+        (51,100,54)
     )
 
     arcade.finish_render()
